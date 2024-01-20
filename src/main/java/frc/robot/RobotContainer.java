7e8b67eacd7957cb671cb26a841178f54b028db7
Ameboid <ameboidna@gmail.com>
2024-01-20 10:31:33 -0600

    Finishing Shooter Testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,9 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -18,14 +20,16 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  private final ShooterSubsystem shooterSubsystem;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
+    shooterSubsystem = new ShooterSubsystem();
   }
 
   /**
@@ -38,6 +42,9 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
+
+    driverController.a().whileTrue(new InstantCommand(() -> shooterSubsystem.runMotorForward()));
+    driverController.b().whileTrue(new InstantCommand(() -> shooterSubsystem.runMotorBackward()));
 
   }
 
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,45 +4,134 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+
+
+/*
+ * NOTE: The Angle Neo is unused!!!!! Please veryify you have stuff set up for it!
+ */
 public class ShooterSubsystem extends SubsystemBase {
     public static class ShooterConstants{
-      public static final int TOP_SHOOTER_CAN_ID = 22;
-      public static final int BOTTOM_SHOOTER_CAN_ID = 23;
-    }
+      public static final int TOP_SHOOTER_CAN_ID = 30;
+      public static final int BOTTOM_SHOOTER_CAN_ID = 31;
+      public static final int ANGLE_MOTOR_CAN_ID = 32;
 
+      public static final int FLEX_RPM_PER_VOLT = 560;
+    }
+    //Creating a PDH object for volatge measurments
+    private PowerDistribution PDH = new PowerDistribution(1, ModuleType.kRev);
+    //Creating the objects for the motors and their encoders, respectivly
     private CANSparkFlex topShooter;
     private CANSparkFlex bottomShooter;
 
+    private CANSparkMax angleShooter;
+
     private static RelativeEncoder topEncoder;
     private static RelativeEncoder bottomEncoder;
+    
+    private static RelativeEncoder angleEncoder;
 
   /** Creates a new ShooterSubsystem. */
   public ShooterSubsystem() {
+    //Setting their CAN ID and the type
     topShooter = new CANSparkFlex(ShooterConstants.TOP_SHOOTER_CAN_ID, MotorType.kBrushless);
     bottomShooter = new CANSparkFlex(ShooterConstants.BOTTOM_SHOOTER_CAN_ID, MotorType.kBrushless);
+    angleShooter = new CANSparkMax(ShooterConstants.ANGLE_MOTOR_CAN_ID, MotorType.kBrushless);
 
+    //Reseting them to defaults
     topShooter.restoreFactoryDefaults();
     bottomShooter.restoreFactoryDefaults();
-    
+    angleShooter.restoreFactoryDefaults();
+
+    //Setting the encoders
     topEncoder = topShooter.getEncoder();
     bottomEncoder = bottomShooter.getEncoder();
+    angleEncoder = angleShooter.getEncoder();
 
+    //Setting the motors idel mode. Coast means they will be free to rotate
     topShooter.setIdleMode(CANSparkFlex.IdleMode.kCoast);
     bottomShooter.setIdleMode(CANSparkFlex.IdleMode.kCoast);
+    angleShooter.setIdleMode(CANSparkMax.IdleMode.kBrake);
+
 
     //TODO verfiy inverts
     topShooter.setInverted(false);
-
     topShooter.setSmartCurrentLimit(60);
+
+
+    angleShooter.setInverted(false);
+    angleShooter.setSmartCurrentLimit(60);
 
     //TODO verify following is working properly
     bottomShooter.follow(topShooter, true);
 
+    //Creating the Shuffleboard tab for testing
+    Shuffleboard.getTab("Shooter Subsystem").add(this);
+  }
+
+  double RPM;
+  /**
+   * Converts the given RPM from RPM to a value between 0 and 1.
+   * @param rpm The set RPM, gotten from the Shuffleboard
+   */
+  public void setRPM(double rpm){
+    if(rpm >= 1.0){ RPM /= (getPDHVoltage() * ShooterConstants.FLEX_RPM_PER_VOLT); }
+  }
+
+  /**
+   * Gets the current RPM of the motor
+   * @return RPM
+   */
+  public double getTopMotorRPM(){
+    double RPM = topEncoder.getVelocity();
+    RPM *= getPDHVoltage() * ShooterConstants.FLEX_RPM_PER_VOLT;
+    return RPM;
+  }
+  /**
+   * Gets the current RPM of the motor
+   * @return RPM
+   */
+  public double getBottomMotorRPM(){
+    double RPM = bottomEncoder.getVelocity();
+    RPM *= getPDHVoltage() * ShooterConstants.FLEX_RPM_PER_VOLT;
+    return RPM;
+  }
+  /**
+   * Runs the motor forward
+   */
+  public void runMotorForward(){
+    topShooter.set(RPM);
+  }
+  /**
+   * Runs the motor backward
+   */
+  public void runMotorBackward(){
+    topShooter.set(-RPM);
+  }
+  /**
+   * Gets the voltage of the PDH Bus for calculations
+   * @return PDH.getVoltage();
+   */
+  private double getPDHVoltage(){
+    return PDH.getVoltage();
+  }
+
+  public void initSendable(SendableBuilder builder){
+    builder.setSmartDashboardType("ShooterSubsystem");
+
+    builder.addDoubleProperty("Set Motor Percent or RPM", null, this::setRPM);
+    builder.addDoubleProperty("Top Motor RPM", this::getTopMotorRPM, null);
+    builder.addDoubleProperty("Bottom Motor RPM", this::getBottomMotorRPM, null);
+    builder.addDoubleProperty("PDH Volatage", this::getPDHVoltage, null);
   }
 
   @Override
