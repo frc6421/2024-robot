e4b9b01cab1b3829761a7f43c5a07ade698a47c8
Aidan <27bruchai@muskegonorwayschools.org>
2024-01-25 19:24:27 -0600

    made isNoteIntransition method

--- src/main/java/frc/robot/subsystems/TransitionSubsystem.java
+++ src/main/java/frc/robot/subsystems/TransitionSubsystem.java
@@ -5,26 +5,38 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TransitionSubsystem extends SubsystemBase {
   
   private final CANSparkMax transitionMotor;
   public static class TransitionConstants {
-    private static final int TRANSITION_MOTOR_CAN_ID = 21;
+    
+    private static final int TRANSITION_MOTOR_CAN_ID = 21;  
+    private static final int TOP_PROXIMITY_SENSOR_DIO = 0;  
+    private static final int BOTTOM_PROXIMITY_SENSOR_DIO = 0; 
+    private static final int PROXIMITY_THRESHOLD = 150;
   }
+  private final DigitalInput topProximitySensor;
+  private final DigitalInput bottomProximitySensor;
   /** Creates a new TransitionSubsystem. */
   public TransitionSubsystem() {
     // Make new instance of motor
     transitionMotor = new CANSparkMax(TransitionConstants.TRANSITION_MOTOR_CAN_ID, MotorType.kBrushless);
   
+    topProximitySensor = new DigitalInput(TransitionConstants.TOP_PROXIMITY_SENSOR_DIO);
+    bottomProximitySensor = new DigitalInput(TransitionConstants.BOTTOM_PROXIMITY_SENSOR_DIO);
+    
     // Factory default
     transitionMotor.restoreFactoryDefaults();
 
     // Set to idle to coast
     transitionMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
+
   }
   /** Sets the belts to the Intake mode
    * 
@@ -34,10 +46,23 @@ public class TransitionSubsystem extends SubsystemBase {
     transitionMotor.set(value);
   }
 
-  /**Sets the belts to the Scoring mode*/
-  public void SetScoringState() {}
+  /**
+   * Comparing topProximitySenson and bottomProximitySensor boolean values
+   * @return true/false
+  */
+  public boolean isNoteInTransition() {
+    if(topProximitySensor.get() == true && bottomProximitySensor.get() == true) {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 }
+
