336a1c92a63ea78a4b43b3a5910d3d4b8af0ef69
Ameboid <ameboidna@gmail.com>
2024-01-27 14:37:11 -0600

    1/27/24: Code Review - Before the major changes

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -10,8 +10,6 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkFlex;
 
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 
@@ -22,14 +20,11 @@ public class ShooterSubsystem extends SubsystemBase {
     public static class ShooterConstants{
       public static final int TOP_SHOOTER_CAN_ID = 30;
       public static final int BOTTOM_SHOOTER_CAN_ID = 31;
-
-      public static final int FLEX_RPM_PER_VOLT = 560;
+      public static final int CURRENT_LIMIT = 60;
     }
-    //Creating a PDH object for volatge measurments
-    private PowerDistribution PDH = new PowerDistribution(1, ModuleType.kRev);
     //Creating the objects for the motors and their encoders, respectivly
-    private CANSparkFlex topShooter;
-    private CANSparkFlex bottomShooter;
+    private CANSparkFlex topShooterMotor;
+    private CANSparkFlex bottomShooterMotor;
 
 
     private static RelativeEncoder topEncoder;
@@ -39,37 +34,41 @@ public class ShooterSubsystem extends SubsystemBase {
   public ShooterSubsystem() {
 
     //Setting their CAN ID and the type
-    topShooter = new CANSparkFlex(ShooterConstants.TOP_SHOOTER_CAN_ID, MotorType.kBrushless);
-    bottomShooter = new CANSparkFlex(ShooterConstants.BOTTOM_SHOOTER_CAN_ID, MotorType.kBrushless);
+    topShooterMotor = new CANSparkFlex(ShooterConstants.TOP_SHOOTER_CAN_ID, MotorType.kBrushless);
+    bottomShooterMotor = new CANSparkFlex(ShooterConstants.BOTTOM_SHOOTER_CAN_ID, MotorType.kBrushless);
 
     //Reseting them to defaults
-    topShooter.restoreFactoryDefaults();
-    bottomShooter.restoreFactoryDefaults();
+    topShooterMotor.restoreFactoryDefaults();
+    bottomShooterMotor.restoreFactoryDefaults();
 
     //Setting the encoders
-    topEncoder = topShooter.getEncoder();
-    bottomEncoder = bottomShooter.getEncoder();
+    topEncoder = topShooterMotor.getEncoder();
+    bottomEncoder = bottomShooterMotor.getEncoder();
+
+    //TODO: Add PID controller
 
     //Setting the motors idel mode. Coast means they will be free to rotate
-    topShooter.setIdleMode(CANSparkFlex.IdleMode.kCoast);
-    bottomShooter.setIdleMode(CANSparkFlex.IdleMode.kCoast);
+    topShooterMotor.setIdleMode(CANSparkFlex.IdleMode.kCoast);
+    bottomShooterMotor.setIdleMode(CANSparkFlex.IdleMode.kCoast);
 
-    topShooter.setInverted(false);
-    topShooter.setSmartCurrentLimit(60);
-
-    bottomShooter.setInverted(true);
-    bottomShooter.setSmartCurrentLimit(60);
+    //TODO: MAKE IT A FOLLOWER!!!!!!!!!!!!!!!!!!
+    topShooterMotor.setInverted(false);
+    topShooterMotor.setSmartCurrentLimit(ShooterConstants.CURRENT_LIMIT);
+    bottomShooterMotor.follow(topShooterMotor, true);
 
     //Creating the Shuffleboard tab for testing
     Shuffleboard.getTab("Shooter Subsystem").add(this);
   }
 
+  // Used for setting the percent output of other functions
   double percentOutput;
 
   /**
-   * Converts the given RPM from RPM to a value between 0 and 1.
-   * @param output The set RPM, gotten from the Shuffleboard
+   * Takes the percent output of the motors and to allow for other 
+   * functions to use it.
+   * @param output The set percent, gotten from Shuffleboard
    */
+  //TODO: Change from Percent output to Velocity
   public double setPercentOutput(double output){
     percentOutput = output;
     return output;
@@ -83,6 +82,7 @@ public class ShooterSubsystem extends SubsystemBase {
     double rpm = topEncoder.getVelocity();
     return rpm;
   }
+
   /**
    * Gets the current RPM of the motor
    * @return rpm
@@ -91,26 +91,14 @@ public class ShooterSubsystem extends SubsystemBase {
     double rpm = bottomEncoder.getVelocity();
     return rpm;
   }
+
   /**
-   * Runs the motor forward
+   * Runs the motor forward with the desired output 
+   * percent
    */
+  //TODO: Change to Velocity stuff and combine Methods 
   public void runMotorForward(){
-    topShooter.set(percentOutput);
-    bottomShooter.set(percentOutput);
-  }
-  /**
-   * Runs the motor backward
-   */
-  public void runMotorBackward(){
-    topShooter.set(-percentOutput);
-    bottomShooter.set(-percentOutput);
-  }
-  /**
-   * Gets the voltage of the PDH Bus for calculations
-   * @return PDH.getVoltage();
-   */
-  private double getPDHVoltage(){
-    return PDH.getVoltage();
+    topShooterMotor.set(percentOutput);
   }
 
   public void initSendable(SendableBuilder builder){
@@ -119,7 +107,6 @@ public class ShooterSubsystem extends SubsystemBase {
     builder.addDoubleProperty("Set Motor Percent", null, this::setPercentOutput);
     builder.addDoubleProperty("Top Motor RPM", this::getTopMotorRPM, null);
     builder.addDoubleProperty("Bottom Motor RPM", this::getBottomMotorRPM, null);
-    builder.addDoubleProperty("PDH Voltage", this::getPDHVoltage, null);
   }
 
   @Override
