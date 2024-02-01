db3efaa74e38a4beb05031fa790517ab3bc1d542
KeiraP12 <petersenkeira1@gmail.com>
2024-01-31 19:34:15 -0600

    started one piece leave auto

---
+++ src/main/java/frc/robot/commands/onePieceLeave.java
@@ -0,0 +1,77 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.List;
+
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.TrajectoryConstants;
+import frc.robot.subsystems.DriveSubsystem;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class onePieceLeave extends SequentialCommandGroup {
+  private DriveSubsystem driveSubsystem;
+
+  /** Creates a new onePieceLeave. */
+  public onePieceLeave(DriveSubsystem drive) {
+
+    driveSubsystem = drive;
+    addRequirements(driveSubsystem);
+
+    TrajectoryConfig forwardConfig = new TrajectoryConfig(
+        AutoConstants.AUTO_MAX_VELOCITY_METERS_PER_SECOND + 0.5,
+        AutoConstants.AUTO_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED)
+        .setKinematics(driveSubsystem.kinematics);
+    
+    TrajectoryConfig reverseConfig = new TrajectoryConfig(
+        AutoConstants.AUTO_MAX_VELOCITY_METERS_PER_SECOND + 0.5,
+        AutoConstants.AUTO_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED + 1.5)
+        .setKinematics(driveSubsystem.kinematics)
+        .setReversed(true);
+
+    Trajectory leaveStartZoneTrajectory = TrajectoryGenerator.generateTrajectory(List.of(
+        new Pose2d(TrajectoryConstants.ONE_PIECE_START, new Rotation2d(0)),
+        new Pose2d(TrajectoryConstants.ONE_PIECE_SHOOT.plus(new Translation2d(0, Units.feetToMeters(0))), new Rotation2d(0))), forwardConfig);
+    
+    var thetaController = new ProfiledPIDController(
+        AutoConstants.THETA_P, AutoConstants.THETA_I, AutoConstants.THETA_D,
+        new TrapezoidProfile.Constraints(AutoConstants.AUTO_MAX_ANGULAR_VELOCITY_RAD_PER_SEC,
+            AutoConstants.AUTO_MAX_ANGULAR_ACCELERATION_RAD_PER_SEC));
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+    HolonomicDriveController holonomicDriveController = new HolonomicDriveController(
+        // Position controllers
+        new PIDController(AutoConstants.X_DRIVE_P, AutoConstants.X_DRIVE_I, AutoConstants.X_DRIVE_D),
+        new PIDController(AutoConstants.Y_DRIVE_P, AutoConstants.Y_DRIVE_I, AutoConstants.Y_DRIVE_D),
+        thetaController);
+
+    SwerveControllerCommand leaveStartZoneCommand = new SwerveControllerCommand(
+        leaveStartZoneTrajectory,
+        driveSubsystem::getPose2d,
+        driveSubsystem.kinematics,
+        holonomicDriveController,
+        driveSubsystem::autoSetModuleStates,
+        driveSubsystem);
+
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands();
+  }
+}
