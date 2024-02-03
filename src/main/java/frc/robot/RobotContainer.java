6f51a4f41a3c88f18be5f940985e603b1ae33670
KierstinS <24schlevenskyki@muskegonorwayschools.org>
2024-02-03 16:07:22 -0600

    Working drive subsystem and drive command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,11 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.DriveCommand;
+import frc.robot.subsystems.DriveSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,12 +19,28 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  // Controllers \\
+  private final CommandXboxController driverController; 
+
+  private static final int driverControllerPort = 0;
+
+  // Subsystems \\
+  private final DriveSubsystem driveSubsystem;
+
+  // Command \\
+  private final DriveCommand driveCommand;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+    driverController = new CommandXboxController(driverControllerPort);
+
+    driveSubsystem = new DriveSubsystem();
+
+    driveCommand = new DriveCommand(driveSubsystem, driverController);
+
+    driveSubsystem.setDefaultCommand(driveCommand);
+    
     // Configure the trigger bindings
     configureBindings();
   }
