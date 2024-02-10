b6d931e4deae3c333d627c746ecee558d063df9e
Ameboid <ameboidna@gmail.com>
2024-02-10 11:50:37 -0600

    Just some setting up.

--- src/main/java/frc/robot/subsystems/CANdleSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANdleSubsystem.java
@@ -7,30 +7,27 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
 import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.RgbFadeAnimation;
+import com.ctre.phoenix.led.TwinkleAnimation;
 
 /** Add your docs here. */
 public class CANdleSubsystem {
     public static class CANdleConstants{
         public static int CANDLE_CAN_ID = 0;
         public static int NUMBER_OF_LED = 0;
+        public static double ANIMATION_SPEED = 1.00; // Percent
+        public static double LED_BRIGHTNESS = 1.00;  // Percent
         
-        //All of the different color combinations we COULD have and want
-        public static enum colors{
-            RED,                    ORANGE,                 YELLOW, //Reds
-            LIME,                   YELLOW_GREEN,           GREEN,  //Greens
-            LIGHT_BLUE,             CYAN,                   BLUE,   //Blues 
-            VIOLET,                 MAGENTA,                PINK,   //Purples
-            WHITE,
-        }
-        //All the different animations that the CANdle can support
-        public static enum animations{
-            RAINBOW,            // Goes through all colors
-            SPARKLE,            // Has a background color and a color that can flicker
-            FADE,               // Fades the selected color
-            FADE_TO_PERCENT,    // Puts the color to a level of opacity      
-            FADE_RAINBOW,       // Rainbow patter that fades
-            SOLID,              // Sets a color to display
-        }
+        //Color combinations. In RGB.
+        //TODO: Get the color values for all of them
+        public static int COLORS[][] = {
+            {255, 0,   0},/* Red */     {0,   0,   0},/* Orange */      {0,   0,   0},/* Yellow */
+            {0,   0,   0},/* Lime */    {0,   0,   0},/* Light Green */ {0, 255,   0},/* Green */
+            {0,   0,   0},/* Cyan */    {0,   0,   0},/* Light Blue */  {0,   0, 255},/* Blue */
+            {0,   0,   0},/* Violet */  {0,   0,   0},/* Magenta */     {0,   0,   0},/* Pink */
+            {255, 255, 255},/* White */ {0,   0,   0},/* Black */
+        };
     }
 
     public CANdle candle = new CANdle(CANdleConstants.CANDLE_CAN_ID);
@@ -48,6 +45,37 @@ public class CANdleSubsystem {
         candle.configV5Enabled(false);
     }
 
+
+    public void setPattern(int color, int animation){
+        switch(animation){
+
+            case 0: // Rainbow
+                RainbowAnimation rainbowAnim = new RainbowAnimation(CANdleConstants.LED_BRIGHTNESS, CANdleConstants.ANIMATION_SPEED, CANdleConstants.NUMBER_OF_LED);
+                candle.animate(rainbowAnim);
+                break;
+
+            case 1: // Rainbow Fade
+                RgbFadeAnimation rainbowFadeAnim = new RgbFadeAnimation(CANdleConstants.LED_BRIGHTNESS, CANdleConstants.ANIMATION_SPEED, CANdleConstants.NUMBER_OF_LED);
+                candle.animate(rainbowFadeAnim);
+                break;
+
+            case 2: // Sparkle with Background
+                TwinkleAnimation twinkleAnim = new TwinkleAnimation(setColorR(color), animation, animation, animation, color, animation, null);
+            case 3: // Fade
+            case 4: // Solid
+        }
+    }
+
+    private int setColorR(int color){
+        return CANdleConstants.COLORS[color][0]; 
+    }
+    private int setColorG(int color){
+        return CANdleConstants.COLORS[color][1]; 
+    }
+    private int setColorB(int color){
+        return CANdleConstants.COLORS[color][2]; 
+    }
+    
     
 
 }
