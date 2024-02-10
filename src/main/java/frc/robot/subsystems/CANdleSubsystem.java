8c3d3ebc355cf4cf163545da26fe643641ebfaf7
Ameboid <ameboidna@gmail.com>
2024-02-10 13:24:38 -0600

    Final CANdle stuff

--- src/main/java/frc/robot/subsystems/CANdleSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANdleSubsystem.java
@@ -9,70 +9,147 @@ import com.ctre.phoenix.led.CANdle.LEDStripType;
 import com.ctre.phoenix.led.CANdleConfiguration;
 import com.ctre.phoenix.led.RainbowAnimation;
 import com.ctre.phoenix.led.RgbFadeAnimation;
+import com.ctre.phoenix.led.SingleFadeAnimation;
 import com.ctre.phoenix.led.TwinkleAnimation;
 
+import edu.wpi.first.util.sendable.SendableBuilder;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 /** Add your docs here. */
-public class CANdleSubsystem {
+public class CANdleSubsystem extends SubsystemBase {
     public static class CANdleConstants{
-        public static int CANDLE_CAN_ID = 0;
-        public static int NUMBER_OF_LED = 0;
-        public static double ANIMATION_SPEED = 1.00; // Percent
+        public static int CANDLE_CAN_ID = 50;
+        public static int NUMBER_OF_LED = 20;
+        public static double ANIMATION_SPEED = 0.50; // Percent
         public static double LED_BRIGHTNESS = 1.00;  // Percent
         
         //Color combinations. In RGB.
-        //TODO: Get the color values for all of them
         public static int COLORS[][] = {
-            {255, 0,   0},/* Red */     {0,   0,   0},/* Orange */      {0,   0,   0},/* Yellow */
-            {0,   0,   0},/* Lime */    {0,   0,   0},/* Light Green */ {0, 255,   0},/* Green */
-            {0,   0,   0},/* Cyan */    {0,   0,   0},/* Light Blue */  {0,   0, 255},/* Blue */
-            {0,   0,   0},/* Violet */  {0,   0,   0},/* Magenta */     {0,   0,   0},/* Pink */
-            {255, 255, 255},/* White */ {0,   0,   0},/* Black */
+            {255,   0,   0},/* Red */     {255, 165,   0},/* Orange */      {255, 255,   0},/* Yellow */
+            { 50, 205,  50},/* Lime */    {144, 238, 144},/* Light Green */ {  0, 255,   0},/* Green */
+            {  0, 255, 255},/* Cyan */    {173, 216, 230},/* Light Blue */  {  0,   0, 255},/* Blue */
+            {127,   0, 255},/* Violet */  {255,   0, 255},/* Magenta */     {255, 193, 203},/* Pink */
+            {255, 255, 255},/* White */   {  0,   0,   0},/* Black */
         };
     }
 
     public CANdle candle = new CANdle(CANdleConstants.CANDLE_CAN_ID);
+
+    public int mainPrimaryColor;
+    public int mainSecondaryColor;
+    public int mainPattern;
 
     public CANdleSubsystem() {
         //Set the CANdle to configure settings
         CANdleConfiguration config = new CANdleConfiguration();
         //Setting the strip connected to RGB
         config.stripType = LEDStripType.RGB;
+        //Setting the brighness of the LED's for solid colors
+        config.brightnessScalar = CANdleConstants.LED_BRIGHTNESS;
         //"Burn"(it doesn't) the settings to the CANdle
         candle.configAllSettings(config);
 
         candle.clearStickyFaults();
         //Turning off the 5V rail of the CANdle output
         candle.configV5Enabled(false);
+
+        Shuffleboard.getTab("CANdle Testing").add(this);
     }
 
-
-    public void setPattern(int color, int animation){
+    /**
+     * Sets the color of the LED strips connected to the CANdle
+     * @param primaryColor The color to be used as the base color. The color and
+     * the number they corospond to is listed:
+     * --0: Red
+     * --1: Orange
+     * --2: Yellow
+     * --3: Lime
+     * --4: Light Green
+     * --5: Green
+     * --6: Cyan
+     * --7: Light Blue
+     * --8: Blue
+     * --9: Violet
+     * --10: Magenta
+     * --11: Pink
+     * --12: White
+     * --13: Black
+     * @param secondaryColor The color to be used in the Twinkle pattern as the pattern
+     * These colors also come from the list for primaryColor
+     * @param animation The animation wanted. The pattern and the number they corespond
+     * to is listed:
+     * --0: Rainbow
+     * --1: Rainbow Fade
+     * --2: Twinkle; Requires a primary and secondary color
+     * --3: Fade; Requires primary only
+     * --4: Solid; Requires primary only
+     */
+    public void setPattern(int primaryColor, int secondaryColor, int animation){
         switch(animation){
 
             case 0: // Rainbow
-                RainbowAnimation rainbowAnim = new RainbowAnimation(CANdleConstants.LED_BRIGHTNESS, CANdleConstants.ANIMATION_SPEED, CANdleConstants.NUMBER_OF_LED);
+                RainbowAnimation rainbowAnim = new RainbowAnimation(CANdleConstants.LED_BRIGHTNESS, 
+                    CANdleConstants.ANIMATION_SPEED, CANdleConstants.NUMBER_OF_LED);
                 candle.animate(rainbowAnim);
                 break;
 
             case 1: // Rainbow Fade
-                RgbFadeAnimation rainbowFadeAnim = new RgbFadeAnimation(CANdleConstants.LED_BRIGHTNESS, CANdleConstants.ANIMATION_SPEED, CANdleConstants.NUMBER_OF_LED);
+                RgbFadeAnimation rainbowFadeAnim = new RgbFadeAnimation(CANdleConstants.LED_BRIGHTNESS, 
+                    CANdleConstants.ANIMATION_SPEED, CANdleConstants.NUMBER_OF_LED);
                 candle.animate(rainbowFadeAnim);
                 break;
 
             case 2: // Sparkle with Background
-                TwinkleAnimation twinkleAnim = new TwinkleAnimation(setColorR(color), animation, animation, animation, color, animation, null);
+                TwinkleAnimation twinkleAnim = new TwinkleAnimation(getColorR(secondaryColor), getColorG(secondaryColor),
+                    getColorB(secondaryColor), 0, CANdleConstants.ANIMATION_SPEED, CANdleConstants.NUMBER_OF_LED, null);
+                // Setting the background color before the pattern
+                candle.setLEDs(getColorR(primaryColor), getColorG(primaryColor), getColorB(primaryColor));
+                candle.animate(twinkleAnim);
+                break;
+                
             case 3: // Fade
+                SingleFadeAnimation fadeAnim = new SingleFadeAnimation(getColorR(primaryColor), getColorG(primaryColor), 
+                    getColorB(primaryColor), 0, CANdleConstants.ANIMATION_SPEED, CANdleConstants.NUMBER_OF_LED);
+                candle.animate(fadeAnim);
+                break;
+
             case 4: // Solid
+                candle.setLEDs(getColorR(primaryColor), getColorG(primaryColor), getColorB(primaryColor));
+                break;
+            
+            default:
+                break;
+
         }
     }
 
-    private int setColorR(int color){
+    public void setPrimaryColor(long color){
+        mainPrimaryColor = (int)color;
+    }
+    public void setSecondaryColor(long color){
+        mainSecondaryColor = (int)color;
+    }
+    public void setPattern(long pattern){
+        mainPattern = (int)pattern;
+    }
+
+    public void initSendable(SendableBuilder builder){
+        builder.setSmartDashboardType("CANdleSubsystem");
+
+        builder.addIntegerProperty("Set Primary Color", null, this::setPrimaryColor);
+        builder.addIntegerProperty("Set Secondary Color", null, this::setSecondaryColor);
+        builder.addIntegerProperty("Pattern", null, this::setPattern);
+    }
+
+    private int getColorR(int color){
         return CANdleConstants.COLORS[color][0]; 
     }
-    private int setColorG(int color){
+    private int getColorG(int color){
         return CANdleConstants.COLORS[color][1]; 
     }
-    private int setColorB(int color){
+    private int getColorB(int color){
         return CANdleConstants.COLORS[color][2]; 
     }
     
