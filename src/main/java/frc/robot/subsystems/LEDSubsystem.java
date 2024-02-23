b1ce4377c7cda304e5208950a8fd66c648f7a8e5
Ameboid <ameboidna@gmail.com>
2024-02-22 18:06:16 -0600

    Removal of the "for" loop for setting the LED's

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -4,8 +4,6 @@
 
 package frc.robot.subsystems;
 
-import java.util.concurrent.TimeUnit;
-
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,7 +12,8 @@ import frc.robot.subsystems.LEDSubsystem.LEDConstants.LEDColors;
 public class LEDSubsystem extends SubsystemBase {
 
   public static class LEDConstants {
-    public static int NUMBER_OF_LEDS = 10;
+    //TODO: Tune this value for the number of LED's on the Robot
+    public static int NUMBER_OF_LEDS = 1;
 
     public static enum LEDColors{
       OFF,
@@ -38,6 +37,7 @@ public class LEDSubsystem extends SubsystemBase {
     led.setLength(LEDConstants.NUMBER_OF_LEDS);
     led.setData(ledBuffer);
     led.start();
+
   }
 
   @Override
@@ -49,22 +49,21 @@ public class LEDSubsystem extends SubsystemBase {
    * @param color the color to set them to
    */
   public void setColor(LEDColors color) {
-      for(int i = 0; i < LEDConstants.NUMBER_OF_LEDS; i++) {
-        switch(color){
-          case BLUE:
-            ledBuffer.setRGB(i,0,0,255);
-            break;
+      switch(color){
+        case BLUE:
+          ledBuffer.setRGB(0,0,0,255);
+          break;
 
-          case HOT_PINK:
-            ledBuffer.setRGB(i,0,255,70);
-            break;
+        case HOT_PINK:
+          ledBuffer.setRGB(0,0,255,70);
+          break;
 
-          case OFF:
-            ledBuffer.setRGB(i,0,0,0);
-            break;
-        }
+        case OFF:
+          ledBuffer.setRGB(0,0,0,0);
+          break;
       }
     led.setData(ledBuffer);
   }
+
 }
 
